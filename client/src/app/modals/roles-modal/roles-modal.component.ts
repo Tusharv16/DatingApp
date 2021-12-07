6b903a7176747a18f6f927a4db0dd77ba71ad05d
Tushar Varkhade <Tushar.varkhade2@gmail.com>
2021-12-07 15:58:04 +0530

    End of section 18

--- client/src/app/modals/roles-modal/roles-modal.component.ts
+++ client/src/app/modals/roles-modal/roles-modal.component.ts
@@ -2,16 +2,15 @@ import { Component, EventEmitter, Input, OnInit } from '@angular/core';
 import { BsModalRef } from 'ngx-bootstrap/modal';
 import { User } from 'src/app/_models/user';
 
-
 @Component({
   selector: 'app-roles-modal',
   templateUrl: './roles-modal.component.html',
   styleUrls: ['./roles-modal.component.css']
 })
 export class RolesModalComponent implements OnInit {
-  @Input() updateSelectedRoles =  new EventEmitter();
+  @Input() updateSelectedRoles = new EventEmitter();
   user: User;
-  roles: any[];
+  roles: any[]; 
 
   constructor(public bsModalRef: BsModalRef) { }
 
